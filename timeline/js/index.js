4415dab157708cf28491a9315cc9f1f98936ce07
Dumindu De Silva <Dumindu@CreativeSoftware.com>
2021-02-22 01:02:58 +0530

    devops badge added

--- timeline/js/index.js
+++ timeline/js/index.js
@@ -7,7 +7,7 @@
       img: ".timeline__img"
     };
     selectors.item.eq(0).addClass(selectors.activeClass);
-    selectors.id.css("background-image", "url(" + selectors.item.first().find(selectors.img).attr("src") + ")");
+    selectors.id.css("background-image", "url("+ selectors.item.first().find(selectors.img).attr("src") + ")");
 
     var itemLength = selectors.item.length;
     $(window).scroll(function() {
@@ -19,10 +19,10 @@
         var that = $(this)
         if (i == itemLength - 2 && pos > min + $(this).height() / 2) {
           selectors.item.removeClass(selectors.activeClass);
-          selectors.id.css("background-image", "url(" + selectors.item.last().find(selectors.img).attr('src') + ")");
+          selectors.id.css("background-image", "url("+ selectors.item.last().find(selectors.img).attr('src') + ")");
           selectors.item.last().addClass(selectors.activeClass)
         } else if (pos <= max - 40 && pos >= min) {
-            selectors.id.css("background-image", "url(" + $(this).find(selectors.img).attr('src') + ")");
+            selectors.id.css("background-image", "url("+ $(this).find(selectors.img).attr('src') + ")");
             selectors.item.removeClass(selectors.activeClass);
             $(this).addClass(selectors.activeClass);
           }
